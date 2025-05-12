e80a39b51892a89cd776008598c0eca66057233c
VMR21 <reddymodith@gmail.com>
2025-05-12 15:26:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Leaderboard from './pages/Leaderboard.jsx';
-import Header from './components/Header.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Leaderboard from "./pages/Leaderboard.jsx";
+import Rewards from "./pages/Rewards.jsx";
+import Header from "./components/Header.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/rewards" element={<Rewards />} />
       </Routes>
     </Router>
   </React.StrictMode>
