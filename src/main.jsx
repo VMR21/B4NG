182f9e322e663a4686b384480d06f44467f0ed6a
VMR21 <reddymodith@gmail.com>
2025-05-12 14:28:26 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Leaderboard from "./pages/Leaderboard";
+import Header from "./components/Header";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
