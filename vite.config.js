06c6a901fad0537a42c2cf16da3d1beca941e32c
VMR21 <reddymodith@gmail.com>
2025-05-12 14:34:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-});
-
-
-// index.html
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>B4NG Supreme</title>
-    <link rel="icon" type="image/png" href="/logo.png" />
-  </head>
-  <body class="bg-black">
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
+  plugins: [react()]
+})
